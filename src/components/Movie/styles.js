c533102ff64fe3be16de9740ba487b48bad2e43e
kTz1 <catalin_simon90@yahoo.com>
2022-07-01 14:39:02 +0300

    configured redux query functionality and displaying the movies

---
+++ src/components/Movie/styles.js
@@ -0,0 +1,38 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles((theme) => ({
+  movie: {
+    padding: '10px',
+  },
+  links: {
+    alignItems: 'center',
+    fontWeight: 'bolder',
+    textDecoration: 'none',
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    '&:hover': {
+      cursor: 'poiter',
+      textDecoration: 'none',
+    },
+  },
+  image: {
+    borderRadius: '20px',
+    height: '300px',
+    marginBottom: '10px',
+    '&:hover': {
+      transform: 'scale(1.05)',
+    },
+  },
+  title: {
+    color: theme.palette.text.primary,
+    textOverflow: 'ellipsis',
+    width: '230px',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    marginTop: '10px',
+    marginBottom: 0,
+    textAlign: 'center',
+  },
+}));
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
 
 const Movies = () => {
-  console.log('Movies');
+  const { data, error, isFetching } = useGetMoviesQuery();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for somting else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occured!';
+
   return (
     <div>
-      Movies
+      <MovieList movies={data} />
     </div>
   );
 };
